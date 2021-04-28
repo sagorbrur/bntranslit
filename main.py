78137819dfa02a43214b735f611f584935091f5e
sagorbrur <sagorhem3532@gmail.com>
2021-04-29 00:35:12 +0600

    main.py removed

--- main.py
+++
@@ -1,7 +0,0 @@
-from bntranslit import BNTransliteration
-
-bntrans = BNTransliteration()
-
-model_path = "/home/sagor/learning/transliterate/Training_bn/weights/Training_bn_model.pth"
-output = bntrans.predict(model_path, 'ami')
-print(output)
