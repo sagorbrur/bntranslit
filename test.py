a109eae4f8cc685616536d8711730d6a3fb6e67a
sagorbrur <sagorhem3532@gmail.com>
2021-05-05 20:03:01 +0600

    v2.1.0 ready for build

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from bntranslit.bntransliteration import BNTransliteration
 
-bntrans = BNTransliteration('model/bntranslit_model.pth')
-# bntrans = BNTransliteration()
+# bntrans = BNTransliteration('model/bntranslit_model.pth')
+bntrans = BNTransliteration()
 
 output = bntrans.predict('aami', topk=10)
 print(output)
